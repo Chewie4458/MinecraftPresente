863d3f5b9d34122d38761716b960bb0353fb9bf7
AlexFilh0 <92822217+AlexFilh0@users.noreply.github.com>
2021-10-21 10:16:32 -0300

    Update Senhas.java

--- app/src/main/java/com/curso/minecraftpresente/Senhas.java
+++ app/src/main/java/com/curso/minecraftpresente/Senhas.java
@@ -3,6 +3,7 @@ package com.curso.minecraftpresente;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,8 +26,9 @@ public class Senhas extends AppCompatActivity {
 
     private Button btnEnter;
 
+    //Lista com as perguntas e respostas
     String[] perguntas = {"Pergunta 1", "Pergunta 2", "Pergunta 3", "Pergunta 4"};
-    String[] respostas = {"A", "B", "C", "D"};
+    String[] respostas = {"a", "b", "c", "d"};
 
     private int count = 0;
 
@@ -44,21 +46,25 @@ public class Senhas extends AppCompatActivity {
 
         btnEnter = (Button)findViewById(R.id.btnEnter);
 
+        txtPergunta.setText(perguntas[0]);
         btnEnter.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                if(count == perguntas.length - 1){
+                //Checa se chegou no final de todas as perguntas, e libera as informações da conta
+                if(count == perguntas.length-1){
                     txtPergunta.setVisibility(View.INVISIBLE);
                     txtInfo.setText("Parabéns Gabriel");
                     txtSenha.setVisibility(View.VISIBLE);
                     edtResp.setVisibility(View.INVISIBLE);
                     btnEnter.setVisibility(View.INVISIBLE);
                 }
+                //Caso ainda falte perguntas, o contador aumenta e coloca a nova pergunta na tela
                 else{
-                    if(respostas[count].equals(edtResp.getText().toString())){
+                    if(respostas[count].equals(edtResp.getText().toString().toLowerCase())){
                         count++;
                         txtPergunta.setText(perguntas[count]);
+                        System.out.println(count);
                     }
                 }
             }
